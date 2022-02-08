ef48f7bebff76b82e30bb68721e35445ddbcf92f
Nicolas Van Eenaeme <nicolas@poison.be>
2022-02-09 00:21:45 +0100

    :sparkles: changed the UI to implement the push notification flow

--- App.tsx
+++ App.tsx
@@ -15,21 +15,48 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
+import { Colors, Header } from 'react-native/Libraries/NewAppScreen';
+import Clipboard from '@react-native-clipboard/clipboard';
+import messaging from '@react-native-firebase/messaging';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { useFCM } from './hooks/useFCM';
+
+const Button: React.FC<{
+  title: string;
+  onPress: () => void;
+}> = ({ onPress, title }) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <TouchableOpacity
+      style={[
+        styles.buttonContainer,
+        {
+          backgroundColor: isDarkMode ? Colors.light : Colors.dark,
+        },
+      ]}
+      onPress={() => onPress()}
+    >
+      <Text
+        style={[
+          styles.buttonTitle,
+          {
+            color: isDarkMode ? Colors.dark : Colors.light,
+          },
+        ]}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
 
 const Section: React.FC<{
   title: string;
-}> = ({children, title}) => {
+}> = ({ children, title }) => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -39,7 +66,8 @@ const Section: React.FC<{
           {
             color: isDarkMode ? Colors.white : Colors.black,
           },
-        ]}>
+        ]}
+      >
         {title}
       </Text>
       <Text
@@ -48,7 +76,8 @@ const Section: React.FC<{
           {
             color: isDarkMode ? Colors.light : Colors.dark,
           },
-        ]}>
+        ]}
+      >
         {children}
       </Text>
     </View>
@@ -57,9 +86,21 @@ const Section: React.FC<{
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+  const messagingDetails = useFCM();
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const canAskPermission =
+    messagingDetails.permission ===
+    messaging.AuthorizationStatus.NOT_DETERMINED;
+
+  const permissionDescription: Record<messaging.AuthorizationStatus, string> = {
+    [messaging.AuthorizationStatus.NOT_DETERMINED]: 'Undetermined',
+    [messaging.AuthorizationStatus.PROVISIONAL]: 'Provisional',
+    [messaging.AuthorizationStatus.AUTHORIZED]: 'Authorized',
+    [messaging.AuthorizationStatus.DENIED]: 'Denied',
   };
 
   return (
@@ -67,26 +108,42 @@ const App = () => {
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+        style={backgroundStyle}
+      >
         <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          }}
+        >
+          <Section title="Permission status">
+            The current permission status is{' '}
+            <Text style={styles.highlight}>
+              {permissionDescription[messagingDetails.permission]}
+            </Text>
+            .
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          {canAskPermission && (
+            <Section title="Request permission">
+              <Button
+                title="Request"
+                onPress={() => messaging().requestPermission()}
+              />
+            </Section>
+          )}
+          {messagingDetails.deviceToken && (
+            <Section title="Your FCM Token">
+              <Text style={styles.highlight}>
+                {messagingDetails.deviceToken}
+              </Text>{' '}
+              <Button
+                title="Copy"
+                onPress={() =>
+                  Clipboard.setString(messagingDetails.deviceToken)
+                }
+              />
+            </Section>
+          )}
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -94,6 +151,16 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonTitle: {
+    fontSize: 18,
+    fontWeight: '400',
+    paddingHorizontal: 24,
+    paddingVertical: 8,
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
